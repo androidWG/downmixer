8acab09697461794321ff1bafb3b37ab503de028
Sam Rodrigues <sam.roliveira@proton.me>
2023-01-10 17:28:50 -0500

    Updated test main.py to have a method to test individual downloads

--- main.py
+++ main.py
@@ -1,20 +1,42 @@
+import asyncio
+from pathlib import Path
+
 from spotipy import SpotifyOAuth
 
+import downloader.convert
+from library import Song
 from providers.audio.youtube_music import YouTubeMusicAudioProvider
 from spotify import SpotipyClient
 
+scope = "user-library-read,playlist-read-private"
+spotify = SpotipyClient(auth_manager=SpotifyOAuth(scope=scope))
 
-def test():
-    scope = "user-library-read,playlist-read-private"
-    spotify = SpotipyClient(auth_manager=SpotifyOAuth(scope=scope))
+folder = Path(r"D:\Files\Music")
+provider = YouTubeMusicAudioProvider()
+
+
+def test_playlist():
     saved_tracks = spotify.current_user_saved_tracks_processed()
 
-    provider = YouTubeMusicAudioProvider()
     for t in saved_tracks:
         results = provider.search(t)
-        downloaded = provider.download(results[0])
-        print(downloaded)
+        if results is None or len(results) == 0:
+            continue
+
+        downloaded = provider.download(results[0], folder)
+        converted = asyncio.run(downloader.convert.convert(downloaded))
+        print(converted)
+
+
+def test_song():
+    sp_result = spotify.track("https://open.spotify.com/track/1BnODvOuKbTnAZYkMVzJCL")
+    song = Song.from_spotify(sp_result)
+
+    results = provider.search(song)
+    downloaded = provider.download(results[0], folder)
+    converted = asyncio.run(downloader.convert.convert(downloaded))
+    print(converted)
 
 
 if __name__ == "__main__":
-    test()
+    test_playlist()
