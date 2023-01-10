46aaef8dd67af726d194ab55e374b06f76986e62
Sam Rodrigues <sam.roliveira@proton.me>
2023-01-10 09:00:01 -0500

    Removed downloader from spotify module

--- spotify/__init__.py
+++ spotify/__init__.py
@@ -3,14 +3,12 @@ import logging
 from spotipy import SpotifyOAuth
 
 from spotify.client import SpotipyClient
-from spotify.downloader import Downloader
 
 logger = logging.getLogger("downmixer").getChild(__name__)
 
 
 class Spotify:
     def __init__(self):
-        self._downloader: Downloader | None = None
         self._spotipy_client: SpotipyClient | None = None
 
     def login(self):
@@ -18,5 +16,3 @@ class Spotify:
         logger.debug("Creating SpotifyClient, asking user to log in...")
         self._spotipy_client = SpotipyClient(auth_manager=SpotifyOAuth(scope=scope))
         logger.info("Creaeted spotipy client")
-
-        self._downloader = Downloader()
