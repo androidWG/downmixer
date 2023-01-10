35cb33e77f97dc879fcf620b353201f7521fb844
Sam Rodrigues <sam.roliveira@proton.me>
2023-01-10 00:14:40 -0500

    Added download function and provider_name

--- providers/audio/youtube_music.py
+++ providers/audio/youtube_music.py
@@ -2,25 +2,22 @@ import logging
 from pathlib import Path
 from typing import Optional, List, Dict, Any
 
-import ytmusicapi as ytmusicapi
+import yt_dlp
+import ytmusicapi
 
 import matching
 from library import Artist, Album, Song
 from providers import BaseAudioProvider, ProviderSearchResult
 
-logger = logging.getLogger("peppermint").getChild(__name__)
+logger = logging.getLogger("downmixer").getChild(__name__)
 
 
 def artist_from_ytmusic(data: Dict[str, Any]) -> Artist:
-    return Artist(
-        name=data["name"]
-    )
+    return Artist(name=data["name"])
 
 
 def album_from_ytmusic(data: Dict[str, Any]) -> Album:
-    return Album(
-        name=data["name"]
-    )
+    return Album(name=data["name"])
 
 
 def song_from_ytmusic(data: Dict[str, Any]) -> Song:
@@ -28,7 +25,8 @@ def song_from_ytmusic(data: Dict[str, Any]) -> Song:
         name=data["title"],
         artists=[artist_from_ytmusic(x) for x in data["artists"]],
         album=album_from_ytmusic(data["album"]),
-        duration=data["duration_seconds"]
+        duration=data["duration_seconds"],
+        url=f"https://music.youtube.com/watch?v={data['videoId']}",
     )
 
 
@@ -37,26 +35,26 @@ def search_result_from_ytmusic(original_song, result_song) -> ProviderSearchResu
         provider="ytmusic",
         original_song=original_song,
         result_song=result_song,
-        match_result=matching.match(original_song, result_song)
+        match_result=matching.match(original_song, result_song),
     )
 
 
 class YouTubeMusicAudioProvider(BaseAudioProvider):
+    provider_name = "youtube-music"
+
     def __init__(self, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
         self.client = ytmusicapi.YTMusic()
+        self.youtube_dl = yt_dlp.YoutubeDL({"encoding": "UTF-8", "format": "bestaudio"})
 
-    def download(self, url: str, song: Song) -> Optional[Path]:
-        pass
-
-    def search(self, song: Song) -> Optional[List]:
+    def search(self, song: Song) -> Optional[List[ProviderSearchResult]]:
         if song.isrc:
             query = song.isrc
         else:
             query = song.title
 
         results = self.client.search(query, filter="songs", ignore_spelling=True)
-        
+
         if len(results) == 0:
             return None
 
@@ -65,6 +63,11 @@ class YouTubeMusicAudioProvider(BaseAudioProvider):
             result_song = song_from_ytmusic(r)
             result_objects.append(search_result_from_ytmusic(song, result_song))
 
-        ordered_results = sorted(result_objects, reverse=True, key=lambda x: x.match_result.sum)
+        ordered_results = sorted(
+            result_objects, reverse=True, key=lambda x: x.match_result.sum
+        )
         return ordered_results
 
+    def _download(self, url: str) -> Optional[Path]:
+        downloaded = self.youtube_dl.extract_info(url, download=True)
+        return Path(downloaded["requested_downloads"][0]["filepath"])
