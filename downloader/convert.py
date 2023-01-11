a6ea1432f0ba4a2778075a7b9580a5970ca05a65
Sam Rodrigues <sam.roliveira@proton.me>
2023-01-10 23:12:33 -0500

    Added logging

--- downloader/convert.py
+++ downloader/convert.py
@@ -1,4 +1,5 @@
 import copy
+import logging
 import os
 from pathlib import Path
 
@@ -6,10 +7,14 @@ from ffmpeg import FFmpeg
 
 from providers import Download
 
+logger = logging.getLogger("downmixer").getChild(__name__)
+
 
 async def convert_download(
     downloaded: Download, delete_original: bool = True
 ) -> Download:
+    logger.info("Starting conversion")
+
     # TODO: Check if file already exists
     output = str(downloaded.filename).replace(downloaded.filename.suffix, ".mp3")
     ffmpeg = (
@@ -19,10 +24,24 @@ async def convert_download(
         .output(output, {"b:a": "320k"})
     )
 
+    logger.info("Running ffmpeg")
     await ffmpeg.execute()
     if delete_original:
         os.remove(downloaded.filename)
 
+    logger.debug("Creating copy of download object")
     edited_download = copy.copy(downloaded)
     edited_download.filename = Path(output)
     return edited_download
+
+    @ffmpeg.on("start")
+    def on_start(arguments):
+        logger.debug("Arguments: " + arguments)
+
+    @ffmpeg.on("stderr")
+    def on_stderr(line):
+        logger.error(line)
+
+    @ffmpeg.on("progress")
+    def on_progress(progress):
+        logger.info(progress)
--- downloader/tagging.py
+++ downloader/tagging.py
@@ -1,13 +1,21 @@
+import logging
+
 import mutagen
 
 from providers import Download
 
+logger = logging.getLogger("downmixer").getChild(__name__)
+
 
 def tag_download(download: Download):
+    logger.info(f"Tagging file {download.filename}")
     file = mutagen.File(download.filename, easy=True)
+
+    logger.debug("Deleting old tag information")
     file.delete()
 
     song = download.search_result.original_song
+    logger.debug(f"Filling with info from attached song '{song.title}'")
     file["title"] = song.name
     file["titlesort"] = song.name
     file["tracknumber"] = [song.track_number, song.album.track_count]
@@ -17,4 +25,5 @@ def tag_download(download: Download):
     file["date"] = song.date
     file["originaldate"] = song.date
 
+    logger.info("Saving tag data to file")
     file.save()
