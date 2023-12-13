09505f59de81de214aae4743dc8bbc1dafb51bfc
Anna Rodrigues <anna.roliveira@proton.me>
2023-12-12 22:43:07 -0500

    Fixed logging for ffmpeg

--- downloader/convert.py
+++ downloader/convert.py
@@ -24,6 +24,26 @@ async def convert_download(
         .output(output, {"b:a": "320k"})
     )
 
+    @ffmpeg.on("start")
+    def on_start(arguments):
+        logger.debug("Arguments: " + ", ".join(arguments))
+
+    @ffmpeg.on("stderr")
+    def on_stderr(line):
+        logger.debug(line)
+
+    @ffmpeg.on("progress")
+    def on_progress(progress):
+        logger.info(progress)
+
+    @ffmpeg.on("terminated")
+    def on_terminated():
+        logger.critical("ffmpeg was terminated!")
+
+    @ffmpeg.on("error")
+    def on_error(code):
+        logger.error(f"ffmpeg error code {code}")
+
     logger.info("Running ffmpeg")
     await ffmpeg.execute()
     if delete_original:
@@ -33,15 +53,3 @@ async def convert_download(
     edited_download = copy.copy(downloaded)
     edited_download.filename = Path(output)
     return edited_download
-
-    @ffmpeg.on("start")
-    def on_start(arguments):
-        logger.debug("Arguments: " + arguments)
-
-    @ffmpeg.on("stderr")
-    def on_stderr(line):
-        logger.error(line)
-
-    @ffmpeg.on("progress")
-    def on_progress(progress):
-        logger.info(progress)
