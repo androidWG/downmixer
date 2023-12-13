be0d87d7d81d0bb868f0b2a7a5a273ce43930039
Anna Rodrigues <anna.roliveira@proton.me>
2023-12-12 22:43:07 -0500

    Updated test file

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
-from library import Song, Artist, Album
+from spotipy import SpotifyOAuth
+
 from providers.audio.youtube_music import YouTubeMusicAudioProvider
+from spotify import SpotipyClient
 
 
-def test_search():
-    song = Song(
-        name="soft spot",
-        artists=[Artist(name="piri"), Artist(name="Tommy Villiers")],
-        album=Album(name="froge.mp3"),
-        duration=220,
-        isrc="QZHN52145683",
-        uri="spotify:track:1mtLhZXbQqeU3qugQkuwhk",
-        url="https://open.spotify.com/track/1mtLhZXbQqeU3qugQkuwhk"
-    )
+def test():
+    scope = "user-library-read,playlist-read-private"
+    spotify = SpotipyClient(auth_manager=SpotifyOAuth(scope=scope))
+    saved_tracks = spotify.current_user_saved_tracks_processed()
 
     provider = YouTubeMusicAudioProvider()
-    results = provider.search(song)
-    print(results)
+    for t in saved_tracks:
+        results = provider.search(t)
+        downloaded = provider.download(results[0])
+        print(downloaded)
 
 
-if __name__ == '__main__':
-    test_search()
-
+if __name__ == "__main__":
+    test()
