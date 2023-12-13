244e2f3ed6e8f4cdaa5caa2a66831668845066f8
Anna Rodrigues <anna.roliveira@proton.me>
2023-12-12 22:43:07 -0500

    Changed convert_download to Converter class

--- src/downmixer/file_tools/__init__.py
+++ src/downmixer/file_tools/__init__.py
@@ -1,6 +1,13 @@
 from enum import Enum
 
 
+class Format(Enum):
+    MP3 = "mp3"
+    FLAC = "flac"
+    WAV = "wav"
+    OPUS = "opus"
+
+
 class AudioCodecs(Enum):
     MP4A_40_5 = "mp4a.40.5"
     OPUS = "opus"
--- src/downmixer/file_tools/convert.py
+++ src/downmixer/file_tools/convert.py
@@ -5,51 +5,58 @@ from pathlib import Path
 
 from ffmpeg import FFmpeg
 
+from downmixer.file_tools import Format
 from src.downmixer.providers import Download
 
 logger = logging.getLogger("downmixer").getChild(__name__)
 
 
-async def convert_download(
-    downloaded: Download, delete_original: bool = True
-) -> Download:
-    logger.info("Starting conversion")
+class Converter:
+    def __init__(self, download: Download, format: Format = Format.MP3, bitrate: str = "320k"):
+        self.download = download
+        self.format = format
+        self.bitrate = bitrate
 
-    # TODO: Check if file already exists
-    output = str(downloaded.filename).replace(downloaded.filename.suffix, ".mp3")
-    ffmpeg = (
-        FFmpeg()
-        .option("vn")
-        .input(str(downloaded.filename))
-        .output(output, {"b:a": "320k"})
-    )
+    async def convert(self, delete_original: bool = True) -> Download:
+        logger.info("Starting conversion")
 
-    @ffmpeg.on("start")
-    def on_start(arguments):
-        logger.debug("Arguments: " + ", ".join(arguments))
+        # TODO: Check if file already exists
+        output = str(self.download.filename).replace(
+            self.download.filename.suffix, self.format.value
+        )
+        ffmpeg = (
+            FFmpeg()
+            .option("vn")
+            .input(str(self.download.filename))
+            .output(output, {"b:a": self.bitrate})
+        )
 
-    @ffmpeg.on("stderr")
-    def on_stderr(line):
-        logger.debug(line)
+        @ffmpeg.on("start")
+        def on_start(arguments):
+            logger.debug("Arguments: " + ", ".join(arguments))
 
-    @ffmpeg.on("progress")
-    def on_progress(progress):
-        logger.info(progress)
+        @ffmpeg.on("stderr")
+        def on_stderr(line):
+            logger.debug(line)
 
-    @ffmpeg.on("terminated")
-    def on_terminated():
-        logger.critical("ffmpeg was terminated!")
+        @ffmpeg.on("progress")
+        def on_progress(progress):
+            logger.info(progress)
 
-    @ffmpeg.on("error")
-    def on_error(code):
-        logger.error(f"ffmpeg error code {code}")
+        @ffmpeg.on("terminated")
+        def on_terminated():
+            logger.critical("ffmpeg was terminated!")
 
-    logger.info("Running ffmpeg")
-    await ffmpeg.execute()
-    if delete_original:
-        os.remove(downloaded.filename)
+        @ffmpeg.on("error")
+        def on_error(code):
+            logger.error(f"ffmpeg error code {code}")
 
-    logger.debug("Creating copy of download object")
-    edited_download = copy.copy(downloaded)
-    edited_download.filename = Path(output)
-    return edited_download
+        logger.info("Running ffmpeg")
+        await ffmpeg.execute()
+        if delete_original:
+            os.remove(self.download.filename)
+
+        logger.debug("Creating copy of download object")
+        edited_download = copy.copy(self.download)
+        edited_download.filename = Path(output)
+        return edited_download
