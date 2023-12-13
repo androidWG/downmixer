543871f57120114eae86b18bb17d30b329e6e5fd
Anna Rodrigues <anna.roliveira@proton.me>
2023-12-12 22:43:07 -0500

    Changed classes in providers

--- file_tools/tagging.py
+++ file_tools/tagging.py
@@ -4,7 +4,6 @@ from urllib.request import urlopen
 import mutagen
 from mutagen.id3 import APIC, USLT, ID3
 
-from matching import MatchQuality
 from providers import Download
 
 logger = logging.getLogger("downmixer").getChild(__name__)
@@ -17,40 +16,35 @@ def tag_download(download: Download):
     logger.debug("Deleting old tag information")
     easy_id3.delete()
 
-    if download.search_result.match.quality == MatchQuality.PERFECT:
-        song = download.search_result.original_song
-    else:
-        song = download.search_result.result_song
-
-    logger.debug(f"Filling with info from attached song '{song.title}'")
-    easy_id3["title"] = song.name
-    easy_id3["titlesort"] = song.name
-    easy_id3["artist"] = song.all_artists
-    easy_id3["isrc"] = _return_if_valid(song.isrc)
-    easy_id3["album"] = _return_if_valid(song.album.name)
-    easy_id3["date"] = _return_if_valid(song.date)
-    easy_id3["originaldate"] = _return_if_valid(song.date)
-    easy_id3["albumartist"] = _return_if_valid(song.album.artists)
+    logger.debug(f"Filling with info from attached song '{download.song.title}'")
+    easy_id3["title"] = download.song.name
+    easy_id3["titlesort"] = download.song.name
+    easy_id3["artist"] = download.song.all_artists
+    easy_id3["isrc"] = _return_if_valid(download.song.isrc)
+    easy_id3["album"] = _return_if_valid(download.song.album.name)
+    easy_id3["date"] = _return_if_valid(download.song.date)
+    easy_id3["originaldate"] = _return_if_valid(download.song.date)
+    easy_id3["albumartist"] = _return_if_valid(download.song.album.artists)
     easy_id3["tracknumber"] = [
-        _return_if_valid(song.track_number),
-        _return_if_valid(song.album.track_count),
+        _return_if_valid(download.song.track_number),
+        _return_if_valid(download.song.album.track_count),
     ]
 
     logger.info("Saving EasyID3 data to file")
     easy_id3.save()
 
-    has_cover = song.album.images is not None and len(song.album.images) != 0
-    if song.lyrics or has_cover:
+    has_cover = download.song.album.images is not None and len(download.song.album.images) != 0
+    if download.song.lyrics or has_cover:
         id3 = ID3(download.filename)
 
-        if song.lyrics:
+        if download.song.lyrics:
             logger.debug("Adding lyrics")
             id3["USLT::'eng'"] = USLT(
-                encoding=3, lang="eng", desc="Unsynced Lyrics", text=song.lyrics
+                encoding=3, lang="eng", desc="Unsynced Lyrics", text=download.song.lyrics
             )
 
         if has_cover:
-            url = song.album.images[0]["url"]
+            url = download.song.album.images[0]["url"]
             logger.debug(f"Downloading cover image from URL {url}")
 
             with urlopen(url) as raw_image:
